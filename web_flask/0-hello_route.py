aef9464e9356da355a3bb94b488f48186f1f4b30
Gift Osariemen Omokpamwan <osariemenchantelle6@gmail.com>
2023-10-20 10:32:24 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,9 @@ from flask import Flask
 app = Flask("__name__")
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage', strict_slashes=False)
 def hello():
-    """Return a given string"""
+    """ Display 'Hello HBNB!' """
     return ("Hello HBNB!")
 
 
